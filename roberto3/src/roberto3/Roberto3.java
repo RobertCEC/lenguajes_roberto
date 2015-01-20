c4ed726d1d271118ebdfb184d22964a0ad2fd5c3
RobertCEC <dufflescheese@hotmail.com>
2015-01-20 15:33:58 -0600

    Update Roberto3.java

--- roberto3/src/roberto3/Roberto3.java
+++ roberto3/src/roberto3/Roberto3.java
@@ -1,42 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package roberto3;
 
-/**
- *
- * @author T-107
- */
+package roberto3;
+import javax.swing.JOptionPane;
+
 public class Roberto3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-System.out.println("robertocarloos");
-    }
+public void imc()
+{
+    String num1,num2;
+    int n1,n2,imc;
     
+    num1=JOptionPane.showInputDialog("dame la mas");
+    num2=JOptionPane.showInputDialog("dame la altura");
+    
+    n1=Integer.parseInt(num1);
+    n2=Integer.parseInt(num2);
+    
+    imc=n1/n2*n2;
+    System.out.pintIn(imc);
+    
+}
+
+    public static void main(String[] args) {
+
+    }
 }
 
 
 
 
 
-#include <stdio.h> 
- #include <stdlib.h> 
- int main(void) { 
- int numero1, numero2, numero3; 
- int multiplicacion; 
- printf("masa: "); 
- scanf("%d", &numero1); 
- printf("altura: "); 
- scanf("%d", &numero2); 
- printf("altura: "); 
- scanf("%d", &numero3); 
- imc = numero1 / numero2 * numero3; 
- printf("\nimc: %d\n", imc); 
- getchar(); getchar(); 
- return 0; 
- } 
