fcd628406b0b56e9e345e8e275267edacdfbcee1
RobertCEC <dufflescheese@hotmail.com>
2015-01-26 19:46:46 -0600

    Update Roberto3.java

--- roberto3/src/roberto3/Roberto3.java
+++ roberto3/src/roberto3/Roberto3.java
@@ -1,31 +1,7 @@
-
-package roberto3;
-import javax.swing.JOptionPane;
-
-public class Roberto3 {
-
-public void imc()
-{
-    String num1,num2;
-    int n1,n2,imc;
-    
-    num1=JOptionPane.showInputDialog("dame hola");
-    num2=JOptionPane.showInputDialog("dame la altura");
-    
-    n1=Integer.parseInt(num1);
-    n2=Integer.parseInt(num2);
-    
-    imc=n1/n2*n2;
-    System.out.pintIn(imc);
-    
-}
+public class Roberto {
 
     public static void main(String[] args) {
-
+        System.out.println("roberto");
     }
+    
 }
-
-
-
-
-
